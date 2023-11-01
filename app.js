7e1f1f44e7dc93e69e6a09962c200fc85ee90238
Pandhu Arya <pandhu.munjalindra@gmail.com>
2023-11-01 13:08:33 +0700

    [doc] initial commit

---
+++ app.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
+const dotenv = require("dotenv");
+const methodOverride = require("method-override");
+const passport = require("passport");
+const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
+
+var app = express();
+const PORT = process.env.PORT || 3000;
+dotenv.config({ path: "./config/.env" });
+
+// Passport config
+require("./config/passport")(passport);
+
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.set("view engine", "ejs");
+
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: false,
+    store: new MongoStore({ mongooseConnection: mongoose.connection }),
+  })
+);
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(require("./routes/index"));
+app.use("/auth", require("./routes/auth"));
+app.use(require("./routes/todo"));
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() =>{
+  console.log("Database connected!")
+  app.listen(PORT, console.log(`listening at ${PORT}`));
+})
