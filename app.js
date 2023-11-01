0159cc26169cc4a6b5a8da5a6d685821ad31bf14
Pandhu Arya <pandhu.munjalindra@gmail.com>
2023-11-01 13:33:41 +0700

    fix: add views path

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.set("view engine", "ejs");
+app.set('views', path.join(__dirname, 'views'));
 
 app.use(
   session({
